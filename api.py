4423f015eb246a5da722a5039fbf6e204d930efd
Divyanshu <dgdivyanshu69@gmail.com>
2025-07-23 23:36:48 +0530

    Fix: allow Flask to use dynamic port for Render deployment

--- api.py
+++ api.py
@@ -19,4 +19,6 @@ def predict():
     return jsonify({'prediction': prediction})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
